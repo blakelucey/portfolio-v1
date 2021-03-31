385337e902cfb52b2660c43affb074830f0d6cba
Blake M Lucey <64055920+blakelucey@users.noreply.github.com>
2021-03-31 09:57:13 -0500

    Third Commit

--- src/Main.js
+++ src/Main.js
@@ -1,18 +1,29 @@
 import react from 'react';
 import {BrowserRouter as Router, Route, Switch, Link, Redirect} from 'react-router-dom';
 import React, {Component} from 'react';
-import Home from './web-pages/Home.js';
-import notFoundPage from './web-pages/404notFound.js';
+
+
 //import Signup from '../web-pages/Signup';
 
 //pages
+import Home from './web-pages/Home.js';
+import notFoundPage from './web-pages/404notFound.js';
+import About from './web-pages/About.js';
+import Projects from './web-pages/Projects.js';
+import Contact from './web-pages/Contact.js';
 
 const Main = () => {
   return (
     <Router>
       <Switch>
       <Route exact path='/' component={Home} />
-      <Route component ={notFoundPage} />
+      <Route exact path ='/404notFound' component ={notFoundPage} />
+      <Route exact path ='/About' component={About} />
+      <Route exact path ='/Projects' component={Projects} />
+      <Route exact path ='/Contact' component={Contact} />
+
+
+      <Redirect to='/404notFound' />
       </Switch>
     </Router>
   );
