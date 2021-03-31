9809877dcef74651bd8f700da21690eeeaf02c52
Blake M Lucey <64055920+blakelucey@users.noreply.github.com>
2021-03-31 09:56:27 -0500

    Third Commit

---
+++ src/web-pages/404notFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const notFoundPage = () => {
+  return (
+    <div>
+    <h2> 404 not found! </h2>
+    </div>
+  );
+};
+
+export default notFoundPage;
--- src/web-pages/Contact.js
+++ src/web-pages/Contact.js
@@ -1,5 +1,5 @@
-//import './App.css';
-import './landingPage.css';
+import React from 'react';
+import './Contact.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faHome} from '@fortawesome/free-solid-svg-icons';
 import {faQuestionCircle} from '@fortawesome/free-regular-svg-icons';
@@ -10,10 +10,7 @@ import {faGithub} from '@fortawesome/free-brands-svg-icons';
 import {faLinkedin} from '@fortawesome/free-brands-svg-icons';
 import { Link } from "react-router-dom";
 
-
-
-
-function Home() {
+const Contact = () => {
   return (
     <div>
     <div className='heading'>
@@ -24,7 +21,7 @@ function Home() {
      </div>
 
      <h1 className='intro'>
-     My name's Blake, it's nice to meet you.
+     This is the Contact Page
      </h1>
      <p className='intro2'>I'm an aspiring software engineer, self-taught programmer, and a Team Lead in the field of Healthcare.
      </p>
@@ -37,12 +34,7 @@ function Home() {
      </div>
 
      </div>
-
-
-
-
-
   );
-}
+};
 
-export default Home;
+export default Contact;
