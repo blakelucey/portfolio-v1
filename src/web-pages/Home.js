4a51416a6eae25f78be040c10dfd4853d1ca4cb1
Blake M Lucey <64055920+blakelucey@users.noreply.github.com>
2021-04-12 12:27:27 -0500

    Reduce width of Jumbotron

--- src/web-pages/Home.js
+++ src/web-pages/Home.js
@@ -14,9 +14,24 @@ import Jumbotron from 'react-bootstrap/Jumbotron'
 
 
 
+/*<Navbar bg="light" expand="lg">
+  <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+  <Navbar.Collapse id="basic-navbar-nav">
+    <Nav className="mr-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#link">Link</Nav.Link>
+      <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+      </NavDropdown>*/
 
 function Home() {
   return (
+
     <div className = 'background'>
     <div className='heading'>
      <Button variant = 'dark'><Link to ='/' className = 'Link'><FontAwesomeIcon icon={faHome} size = '4x' className='HomeIcon'/></Link></Button>
